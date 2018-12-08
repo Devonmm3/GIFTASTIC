b0c935658fef4aaca04eff6d16c6ea01c97f7983
DevonMM <Devon.m.moran@gmail.com>
2018-12-08 15:03:35 -0500

    test

---
+++ assets/javascript/app.js
@@ -0,0 +1,95 @@
+var topics = [
+    "It's a wonderful life",
+    "Casablanca",
+    "Citizen Kane",
+    "I love Lucy",
+    "Gone with the Wind",
+    "Miracle on 34th St",
+    "All about Eve",
+    "A Philadelphia Story"
+];
+
+function createButton(text) {
+    $("#buttonsList").append(
+        "<button type='button' class='btn btn-primary buttonList'>" +
+        text +
+        "</button>"
+    );
+}
+//function that shows the keywords from array in the buttons
+function wordToButton() {
+    for (i = 0; i < topics.length; i++) {
+        createButton(topics[i]);
+    }
+}
+//function that moves the gif from the opposite state that it is in (still to animated and vice versa)
+function gifSwitch() {
+    $(".gif").on("click", function () {
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+            $(this).attr("src", $(this.attr("data-animate")));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    });
+}
+//function to generate the gifs when the keywordButton is clicked
+function generateGif(text) {
+    $(".giphyBox").empty();
+    var queryURL =
+        "https://api.giphy.com/v1/gifs/search?q=" +
+        text +
+        "&api_key=kK2dFOB77wwnUfetNfWD5qKQUHx8UBp7&limit=10";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function (response) {
+        for (i = 0; i < 10; i++) {
+            var srcOfGif = response.data[i].images.fixed_height_still.url;
+            var altSrcGif = response.data[i].images.fixed_height.url;
+            var gifRating = response.data[i].rating;
+            $(".giphyBox").append(
+                "<div class='col-md-6'><img src=" +
+                srcOfGif +
+                " data-still" +
+                altSrcGif +
+                "><p class='rating'>Rating: " +
+                gifRating +
+                "</p></div"
+            );
+        }
+    });
+}
+
+function NewButton() {
+    $(".addButton").click(function () {
+        event.preventDefault();
+        var newMovie = $("input")
+            .val()
+            .trim();
+        if (newMovie === "") {
+            return;
+        }
+        topics.push(newMovie);
+        $("#buttonsList").empty();
+        wordToButton();
+        generateGif();
+        $("form")[0].reset();
+    });
+}
+
+function initGetGifsListener() {
+    $('#buttonsList').on('click', '.buttonList', function () {
+        generateGif($(this).text());
+    });
+}
+$(document).ready(function () {
+    console.log("Old Movies");
+    wordToButton();
+    // generateGif();
+    gifSwitch();
+    NewButton();
+    initGetGifsListener();
+});
